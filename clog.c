3bb411d92c34437ebfe20da0daa46a9382ab6003
TynK-M <matteo.pauroso@gmail.com>
2025-08-08 11:13:32 +0200

    Feat(clog): created the structure for the logger

---
+++ clog.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "clog.h"
+
+int main(int argc, char *argv[])
+{
+  CLogger logger;
+  logger = init_clog();
+
+  return 0;
+}
--- clog.h
+++ clog.h
@@ -17,11 +17,56 @@ typedef enum {
   CRITICAL
 } CLog_Level;
 
+/*
+ * CLOG LOGGER:
+ * A basic logging structure for capturing log messages at a specific log level.
+ *
+ * Members:
+ * - level: The current logging level defined by CLog_Level.
+ * - items: Pointer to a dynamically allocated array of log message strings, timestamp, uuid, etc.
+ * - count: The number of items currently hold.
+ */
 typedef struct{
-  *char level;
-  *char items;
+  CLog_Level level;
+  char *items;
   size_t count;
 } CLogger;
 
+char *get_level(CLogger clog){
+  switch (clog.level) {
+    case 0:
+      return "DEBUG";
+    case 1:
+      return "INFO";
+    case 2:
+      return "WARNING";
+    case 3:
+        return "ERROR";
+    case 4:
+        return "CRITICAL";
+    default:
+      break;
+  }
+
+  return "UNWANTED RESULT";
+}
+
+/*
+ * CLOG_OPTIONS:
+ * Set of options to modify pre-defined parts of the logger.
+ */
+#ifdef CLOG_OPTIONS
+
+CLogger set_clog_level(CLogger *clog, CLog_Level level){
+  clog->level = level;
+}
+
+#endif // CLOG_OPTIONS
+
+CLogger init_clog(){
+  CLogger clog;
+  clog.level = INFO;
+  return clog;
+}
 
 #endif // CLOG
