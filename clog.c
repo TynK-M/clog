fbe4246fd73a681c19ec9c748888a7a6ce70f509
TynK-M <matteo.pauroso@gmail.com>
2025-08-25 23:25:53 +0200

    Feat(get_time(), create Clog_Time_Format): add RFC2445, create Clog_Time_Format enum
    Added the possibility of using a simplified version of RFC2445 inside the get_time() function
    
    Made the function use a switch that uses the parameter(the time format, that corresponds to an entry inside the new enum, Clog_Time_Format)

--- clog.c
+++ clog.c
@@ -4,49 +4,72 @@
  */
 
 #define CLOG
-#define RFC822
 
 #include "clog.h"
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
 
-char *get_time(){
+typedef enum {
+  RFC822,
+  RFC2445
+} Clog_Time_Format;
+
+char *get_time(Clog_Time_Format format){
   static char buffer[128];
   time_t now = time(NULL);
 
-#ifdef RFC822 // https://www.rfc-editor.org/rfc/rfc82
-  struct tm local_tm; 
-  struct tm gm_tm;
+  switch(format){
+    case RFC822: { // https://www.rfc-editor.org/rfc/rfc822
+      struct tm local_tm; 
+      struct tm gm_tm;
 
 #if defined(__CYGWIN__) || defined(__unix__) || defined(__APPLE__)
-  localtime_r(&now, &local_tm);
-  gmtime_r(&now, &gm_tm);
+      localtime_r(&now, &local_tm);
+      gmtime_r(&now, &gm_tm);
+#else
+      #error ("Unsupported compiler");
+#endif // System
+
+      long offset = (long)difftime(mktime(&local_tm), mktime(&gm_tm));
+      int sign = (offset < 0) ? -1 : 1;
+      offset = labs(offset);
+      int hours = (int)(offset / 3600);
+      int mins  = (int)((offset % 3600) / 60);
+
+      static const char *wday[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+      static const char *mon[]  = {"Jan","Feb","Mar","Apr","May","Jun",
+        "Jul","Aug","Sep","Oct","Nov","Dec"};
+
+      snprintf(buffer, sizeof(buffer),
+               "%s, %02d %s %04d %02d:%02d:%02d %+03d%02d",
+               wday[local_tm.tm_wday],
+               local_tm.tm_mday, mon[local_tm.tm_mon], local_tm.tm_year + 1900,
+               local_tm.tm_hour, local_tm.tm_min, local_tm.tm_sec,
+               (sign < 0 ? '-' : '+'), hours, mins);
+      return buffer;
+    }
+
+    case RFC2445: {// https://www.rfc-editor.org/rfc/rfc2445 , simplified version without timezone
+      struct tm local_tm;
+#if defined(__CYGWIN__) || defined(__unix__) || defined(__APPLE__)
+      localtime_r(&now, &local_tm);
 #else
   #error ("Unsupported compiler");
 #endif // System
+      snprintf(buffer, sizeof(buffer),
+               "%04d%02d%02dT%02d%02d%02d",
+               local_tm.tm_year + 1900, local_tm.tm_mon + 1, local_tm.tm_mday,
+               local_tm.tm_hour, local_tm.tm_min, local_tm.tm_sec);
+      return buffer;
+    }
 
-  long offset = (long)difftime(mktime(&local_tm), mktime(&gm_tm));
-  int sign = (offset < 0) ? -1 : 1;
-  offset = labs(offset);
-  int hours = (int)(offset / 3600);
-  int mins  = (int)((offset % 3600) / 60);
-
-  static const char *wday[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-  static const char *mon[]  = {"Jan","Feb","Mar","Apr","May","Jun",
-                               "Jul","Aug","Sep","Oct","Nov","Dec"};
-
-  snprintf(buffer, sizeof(buffer),
-           "%s, %02d %s %04d %02d:%02d:%02d %+03d%02d",
-           wday[local_tm.tm_wday],
-           local_tm.tm_mday, mon[local_tm.tm_mon], local_tm.tm_year + 1900,
-           local_tm.tm_hour, local_tm.tm_min, local_tm.tm_sec,
-           (sign < 0 ? '-' : '+'), hours, mins);
-  return buffer;
-#else
-  snprintf(buffer, sizeof(buffer), "%ld", (long)now);
-  return buffer;
-#endif // RFC822  
+    default: {
+      snprintf(buffer, sizeof(buffer), "%ld", (long)now);
+      return buffer;
+      break;
+    }
+  }
 }
 
 int main()
@@ -58,7 +81,7 @@ int main()
   //clog_debug(logger, "That is a debug message");
   //clog_warn(logger, "That is a warning message");
 
-  printf("%s", get_time());
+  printf("%s", get_time(RFC2445));
 
   return 0;
 }
